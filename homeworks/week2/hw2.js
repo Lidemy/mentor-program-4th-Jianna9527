b16d5697079e47edaeef5e79e0d4871cf74bcd84
Jianna <peijia1995@gmail.com>
2020-07-05 00:00:52 +0800

    week2 作業完成

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,11 @@
+//a:97,z:122,A:65
 function capitalize(str) {
-  
+    var firstWord = str[0].charCodeAt(0)
+    if (firstWord >= 97 && firstWord <= 122) {
+        return (str.replace(str[0], String.fromCharCode(firstWord - 32)))
+    } else {
+        return str
+    }
 }
 
 console.log(capitalize('hello'));
