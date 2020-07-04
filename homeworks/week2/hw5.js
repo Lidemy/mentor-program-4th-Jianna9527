f2fd08171363c4efa02f23cdc7c415efc7e95337
Jianna <peijia1995@gmail.com>
2020-07-05 00:00:52 +0800

    week2 作業完成

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,25 @@
 function join(arr, concatStr) {
-  
+    var result = ''
+    if (arr.length > 1) {
+        for (let i = 0; i < arr.length - 1; i++) {
+            result += arr[i] + concatStr
+        }
+        return result + arr[arr.length - 1]
+    } else {
+        return arr
+    }
 }
 
 function repeat(str, times) {
-  
+    var result = ''
+    for (let i = 1; i <= times; i++) {
+        result += str
+    }
+    return result
 }
 
-console.log(join(['a'], '!'));
-console.log(repeat('a', 5));
+//console.log(join(['a'], '!'));
+//console.log(repeat('a', 5));
+console.log(join([1, 2, 3], ''))
+console.log(join(["a", "b", "c"], "!"))
+console.log(join(["aaa", "bb", "c", "dddd"], ',,'))
