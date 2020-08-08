a11b976945dc3e68071706cc6a3e3baaabb06656
Jianna <peijia1995@gmail.com>
2020-08-08 19:47:59 +0800

    week4 hw1 finished

---
+++ homeworks/week4/hw1.js
@@ -0,0 +1,20 @@
+const request = require('request');
+
+const API_URL = 'https://lidemy-book-store.herokuapp.com/books?_limit=10';
+
+request(`${API_URL}/books?_limit=10`, (err, res, body) => {
+  if (err) {
+    console.log('抓取失敗', err);
+    return;
+  }
+  let data;
+  try { // 以防 response 不是一個合法的 JSON 字串
+    data = JSON.parse(body);
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+  for (let i = 0; i < data.length; i++) {
+    console.log(`${data[i].id} ${data[i].name}`);
+  }
+});
